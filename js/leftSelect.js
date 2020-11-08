635d10cffdd6d4edd00874cc817229049b28264c
Lpppsea9 <1638697794@qq.com>
2020-11-08 23:08:09 +0800

    D:/GIT/Git/8

--- js/leftSelect.js
+++ js/leftSelect.js
@@ -3,16 +3,18 @@ let li1 = document.getElementsByClassName("leftSelectTitle");
 for(let i = 0; i < li1.length; i++) {
   let atag = li1[i].children[0];  //a标签
   let ul2 = li1[i].children[1].children[0]; //二级ul
+  let li2 = ul2.getElementsByTagName("li") //二级ul里的li元素
   // console.log(leftSelect_item);
   // console.log(aline);
   atag.addEventListener("click", function () {
-    
-    if(this.parentNode.children[1].style.display == "" || this.parentNode.children[1].style.display == "none"){
+    if (this.parentNode.children[1].style.display == "" || this.parentNode.children[1].style.display == "none"){
       this.parentNode.children[1].style.display = "block";
       this.style.color = "#fff"
-    }else{
+      ul2.style.backgroundColor = "black"
+    } else {
       this.parentNode.children[1].style.display = "none";
-      this.style.color = "#c4c5c9"
+      this.style.color = "#c4c5c9";
+      // ul2.style.backgroundColor = "yellow"
     }
   })
   
@@ -20,8 +22,12 @@ for(let i = 0; i < li1.length; i++) {
   const li2len = ul2.getElementsByTagName("li").length; //二级ul里的li标签个数
   console.log(li2len);
   for (let j = 0; j < li2len; j++) {
+    
     ul2.children[j].addEventListener("click",function () {
-      this.style.backgroundColor = "red"
+      for (let n = 0; n < li2len; n++) {
+        li2[n].style.backgroundColor = "#323642";
+      }
+      ul2.children[j].style.backgroundColor = "red";
     })
   }
 }
